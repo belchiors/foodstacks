90e218a9f6b5a8893646581564434962b708beb4
Mota <eva.marla.c.mota@accenture.com>
2022-05-05 10:24:14 -0300

    test user controller

--- src/test/kotlin/api/foodstacks/controller/UserControllerTest.kt
+++ src/test/kotlin/api/foodstacks/controller/UserControllerTest.kt
@@ -4,59 +4,41 @@ import api.foodstacks.model.User
 import api.foodstacks.service.UserService
 import com.nhaarman.mockito_kotlin.*
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.MockitoAnnotations
 import java.time.LocalDateTime
 
 class UserControllerTest {
+
+    @BeforeEach
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+    }
 
     @InjectMocks
     lateinit var usersController: UsersController
 
     @Mock
-    private lateinit var userService: UserService
+    lateinit var userService: UserService
 
     @Test
-    fun `create an account with success`() {
+    fun `create an user with success`() {
         val userMock = buildUser()
 
         whenever(userService.create(any<User>())) doReturn userMock
-
         val response = usersController.createUser(userMock)
 
         verify(userService, times(1)).create(any<User>())
         assertEquals(userMock.id, response.body?.id)
     }
 
-    @Test
-    fun `create an account with invalid data`() {
-        val userInvalidMock = buildUserInvalid()
-
-        whenever(userService.create(any<User>())).thenThrow(CreateUserBadRequest("erro", 400))
-
-        assertThrows<CreateUserBadRequest> {
-            usersController.createUser(userInvalidMock)
-        }
-    }
-
     private fun buildUser() : User {
         return User(
             id = "1",
             name = "Rafaela Monteiro",
-            email = "montsrafa@cin.ufpe.br",
-            password = "123senha",
-            cpf = "14577898909",
-            birthday = "2000-09-02",
-            createdAt = LocalDateTime.of(2021, 1, 8, 20, 41, 35),
-            updatedAt = LocalDateTime.of(2021, 1, 8, 20, 41, 35)
-        )
-    }
-
-    private fun buildUserInvalid() : User {
-        return User(
-            id = "1",
-            name = "",
             email = "montsrafa@cin.ufpe.br",
             password = "123senha",
             cpf = "14577898909",
