c2a86665b2cc6ceef92e51639ec49da555d29713
Belchior Silva <belchiorisv@gmail.com>
2022-05-05 11:25:34 -0300

    Implementa métodos de retorno de dados da entidade Shop

--- src/main/kotlin/api/foodstacks/controller/ShopController.kt
+++ src/main/kotlin/api/foodstacks/controller/ShopController.kt
@@ -1,7 +1,30 @@
 package api.foodstacks.controller
 
+import api.foodstacks.model.Shop
+import api.foodstacks.service.ShopService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-class ShopController {
+@RequestMapping("/shops")
+class ShopController(private val shopService: ShopService) {
+    @GetMapping
+    fun getAllShops(): ResponseEntity<Iterable<Shop>> {
+        return ResponseEntity(shopService.getAll(), HttpStatus.OK)
+    }
+
+    @PostMapping
+    fun createShop(@RequestBody shop: Shop): ResponseEntity<Any> {
+        return ResponseEntity(shopService.create(shop), HttpStatus.OK)
+    }
+
+    @GetMapping("/{shopId}")
+    fun getShopById(@PathVariable shopId: String): ResponseEntity<Any> {
+        val body = shopService.getShopById(shopId)
+        if (body == null) {
+            return ResponseEntity("Loja não encontrada", HttpStatus.OK)
+        }
+        return ResponseEntity(body, HttpStatus.OK)
+    }
 }
