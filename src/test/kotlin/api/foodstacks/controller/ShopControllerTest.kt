bc16bbd87dbb4e254bdc8194c48e1283625210d8
Belchior Silva <belchiorisv@gmail.com>
2022-05-08 22:37:09 -0300

    Remove imports inutilizados

--- src/test/kotlin/api/foodstacks/controller/ShopControllerTest.kt
+++ src/test/kotlin/api/foodstacks/controller/ShopControllerTest.kt
@@ -1,7 +1,6 @@
 package api.foodstacks.controller
 
 import api.foodstacks.model.Shop
-import api.foodstacks.model.BaseShop
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
